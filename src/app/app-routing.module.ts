e6e8d6962b8992e912c61b09529ef8571a226792
SaneManiacMG <mmoroane@hotmail.com>
2022-12-05 11:58:36 +0200

    Generated child components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ResolveFn, RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
+import { ChildAComponent } from './first/child-a/child-a.component';
 import { SecondComponent } from './second/second.component';
+import { ChildBComponent } from './second/child-b/child-b.component';
 
 const routes: Routes = [
-  { path: 'first-component', component: FirstComponent },
-  { path: 'second-component', component: SecondComponent },
+  {
+    path: 'first-component',
+    component: FirstComponent,
+    children: [
+      {
+        path: 'child-a',
+        component: ChildAComponent
+      }
+    ]
+  },
+  {
+    path: 'second-component',
+    component: SecondComponent,
+    children: [
+      {
+        path: "child-b",
+        component: ChildBComponent
+    }
+    ]
+  },
   { path: '', redirectTo: '/first-component', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent}
+  //{ path: '**', component: PageNotFoundComponent}
 ];
+
+const resolveChildATitle: ResolveFn<string> = () => Promise.resolve('child a');
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule } from '@angular/router';
+import { ChildAComponent } from './first/child-a/child-a.component';
+import { ChildBComponent } from './second/child-b/child-b.component';
+
 @NgModule({
   declarations: [
     AppComponent,
+    ChildAComponent,
+    ChildBComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
