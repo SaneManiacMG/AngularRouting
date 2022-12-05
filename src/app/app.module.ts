c430d8802638fe768a2a9b1842df15704ee82038
SaneManiacMG <mmoroane@hotmail.com>
2022-12-05 10:52:04 +0200

    Moved first and second from app.module to app-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent,
-    SecondComponent
   ],
   imports: [
     BrowserModule,
